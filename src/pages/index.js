7d9a3a71c6670f3decf6a9ba0f60c353b37424a8
Aman Bhargava <56464156+BhargavaCreates@users.noreply.github.com>
2020-08-05 13:53:11 +0530

    google meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,18 @@ import Appbar from "../components/appbar"
 import Info from "../components/info"
 import { Helmet } from "react-helmet"
 import Footer from "../components/footer"
+import MetaTags from 'react-meta-tags';
 
 function index() {
   return (
     <div>
+       <MetaTags>
+            <title>Page 1</title>
+            <meta name="description" content="Some description." />
+            <meta property="og:title" content="MyApp" />
+            <meta property="og:image" content="path/to/image.jpg" />
+            <meta name="google-site-verification" content="ZUD_YcRlDkqjXd2crIbpQpGIYPw7LHZlmXZQ7Yxymsg" />
+          </MetaTags>
       <Appbar />
       <Info />
       <Helmet>
